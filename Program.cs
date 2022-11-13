bcedb149af7db51f0b360f88d90d79c2cde6bb67
Alex <uvarovaas@ya.ru>
2022-11-13 14:12:33 +0700

    добавлен метод - SortArray

--- Program.cs
+++ Program.cs
@@ -12,17 +12,44 @@ void PrintArray (string[] Array)
 {
     for (int i =0; i<Array.Length; i++)
     {
-        if(i==0)  Console.Write($"[{Array[i]}, ");
+        if((i==(Array.Length-1)) && i==0)  Console.Write($"[{Array[i]}] ");
+        else if(i==0)  Console.Write($"[{Array[i]}, ");
         else if(i==(Array.Length-1))  Console.Write($"{Array[i]}] ");
         else Console.Write($"{Array[i]}, ");
     }
 }
 
-
 Console.Write("Введите необходимое количество элементов массива: ");  
-int N = Convert.ToInt32(Console.ReadLine());
-string [] workArray = new string[N];
+int n = Convert.ToInt32(Console.ReadLine());
+string [] workArray = new string[n];
 
 CreateArray (workArray);
 PrintArray (workArray);
 
+Console.WriteLine();
+
+Console.Write("Введите требуемое ограничение по количеству символов для элемента нового массива: ");  
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+void SortArray (string[] Array)
+{
+    for (int i=0; i<Array.Length; i++)
+    {
+        if (Array[i].Length < num+1) count +=1;
+    }
+
+    string[] arrayNew = new string[count];
+    int j =0;
+    for (int i = 0; i<Array.Length; i++)
+    {
+        if(Array[i].Length < num+1)
+        {
+          arrayNew[j]=Array[i];
+          j++;
+        }
+    }
+    PrintArray (arrayNew);
+}
+
+SortArray (workArray);
