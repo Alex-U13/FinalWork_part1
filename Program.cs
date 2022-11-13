6d4724d635ee0fc49b2e9f636048ff3b0fb5753f
Alex <uvarovaas@ya.ru>
2022-11-13 13:16:44 +0700

    начало работы, 2метода

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+void CreateArray (string[] Array)
+{
+        for (int i=0; i<Array.Length; i++)
+        {
+            Console.Write("Введите значение элемента массива: ");
+            Array[i]=Console.ReadLine();
+        }
+}
+void PrintArray (string[] Array)
+{
+    for (int i =0; i<Array.Length; i++)
+    {
+        if(i==0)  Console.Write($"[{Array[i]}, ");
+        else if(i==(Array.Length-1))  Console.Write($"{Array[i]}] ");
+        else Console.Write($"{Array[i]}, ");
+    }
+}
+
+
+Console.Write("Введите необходимое количество элементов массива: ");  
+int N = Convert.ToInt32(Console.ReadLine());
+string [] workArray = new string[N];
+
+CreateArray (workArray);
+PrintArray (workArray);
+
